6b73841bdca18bab1ec90508f94825dfa87dcacc
joaquimsnjunior <joaquimsilvanetojunior@gmail.com>
2025-12-24 20:22:13 -0300

    Automated commit

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -19,8 +19,8 @@ export function Header() {
             priority
           />
           <div className="flex justify-end flex-col sm:flex-row sm:items-center sm:gap-4 w-full">
-            <ScrambleText className="font-semibold sm:text-[2rem] md:text-[2.5rem] leading-none flex-auto" text="~/Joaquim_Silva" />
-            <h3 className="text-[0.80rem] text-gray-400 leading-none sm:flex-wrap mt-2">/Development/joaquimsnjr.tech</h3>
+            <ScrambleText className="font-semibold sm:text-[1.5rem] md:text-[2.5rem] leading-none flex-auto" text="Joaquim Silva" />
+            <h3 className="text-[0.80rem] text-gray-400 leading-none sm:flex-wrap mt-2">~/Development/joaquimsnjr.tech</h3>
           </div>
         </span>
       </h1>
